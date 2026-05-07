cfa2d96a74a94df1a9a643c0f1431eab586ce24e
Rocío Yáñez <rocioyaez99@gmail.com>
2026-05-06 21:42:59 -0400

    test(exercises): add descriptive ids to parametrize in two_sum tests

--- tests/exercises/test_day_01_two_sum.py
+++ tests/exercises/test_day_01_two_sum.py
@@ -30,10 +30,10 @@ class TestTwoSumExamples:
     @pytest.mark.parametrize(
         ("nums", "target", "expected"),
         [
-            ([2, 7, 11, 15], 9, (0, 1)),
-            ([3, 2, 4], 6, (1, 2)),
-            ([3, 3], 6, (0, 1)),
-            ([-1, -2, -3, -4, -5], -8, (2, 4)),
+            pytest.param([2, 7, 11, 15], 9, (0, 1), id="basic_sorted"),
+            pytest.param([3, 2, 4], 6, (1, 2), id="unsorted"),
+            pytest.param([3, 3], 6, (0, 1), id="duplicates_only"),
+            pytest.param([-1, -2, -3, -4, -5], -8, (2, 4), id="all_negatives"),
         ],
     )
     def test_parametrized(
